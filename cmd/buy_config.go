c99ea1f26cff9a89956916f920eaf8438e008697
Patrick Hughes <patrick.hughes@coinbase.com>
2024-03-13 16:56:48 -0500

    add buy config

---
+++ cmd/buy_config.go
@@ -0,0 +1,32 @@
+package cli
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/coinbase-samples/pay-cli/sdk"
+	"github.com/spf13/cobra"
+)
+
+var configCmd = &cobra.Command{
+	Use:   "buyconfig",
+	Short: "Returns a ist of supported countries, payment methods, currencies and crypto by Coinbase Pay",
+	Long:  ConfigDescription,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		resp, err := sdk.Client.BuyConfig(ctx)
+		if err != nil {
+			log.Fatalf("error obtaining buy config %s", err)
+		}
+		ConfigToJson(resp)
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(configCmd)
+}
